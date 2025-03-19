37e2e5179838087293bd4f2b2fbad3e24954617e
Gregory Mbayo <gregory.mbayo@eduvaud.ch>
2025-03-19 10:58:48 +0100

    ajout de l'exercice 1.2

---
+++ Ex1/src/html/HelloWorld.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+    ETML
+    auteur : Gregory Mbayo
+    Date : 19.03.2025
+    Description : Exercice 1.2
+-->
+    <head>
+		<meta charset="utf-8">
+		<title>ex 1.2</title>	
+	</head>
+    <body>
+
+        <h1>Hello World</h1>
+
+    </body>
+
+</html>
